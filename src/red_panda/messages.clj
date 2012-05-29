66457720d3cd0888534b03867153406d9bcc818d
Max Gonzih <gonzih@gmail.com>
2012-05-29 21:03:08 +0300

    add messages fetching function

--- src/red_panda/messages.clj
+++ src/red_panda/messages.clj
@@ -1,16 +1,21 @@
 (ns red-panda.messages
   (:require [monger.core :as mg]
             [monger.collection :as mc]
+            [monger.query :as mq]
             monger.joda-time)
   (:import [com.mongodb MongoOptions ServerAddress]))
 
 (mg/connect!)
 (mg/set-db! (mg/get-db "red-panda"))
 
-(def db "messages")
+(def coll "messages")
+(def per-page 50)
 
 (defn add-message [data]
-  (mc/insert db data))
+  (mc/insert coll data))
 
-(defn get-messages [& [page]]
-  (mc/find-maps db))
+(defn get-messages [& [channel page]]
+  (mq/with-collection coll
+                      (mq/find { :channel channel })
+                      (mq/paginate :page (or page 1) :per-page per-page)
+                      (mq/sort { :time -1})))
