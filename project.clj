25c7e1069372a81397aae80441a1b23c67c7e217
Max Gonzih <gonzih@gmail.com>
2012-05-29 19:37:20 +0300

    add deps

--- project.clj
+++ project.clj
@@ -1,4 +1,7 @@
 (defproject red-panda "1.0.0-SNAPSHOT"
   :description "IRC bot written in clojure"
-  :dependencies [[org.clojure/clojure "1.4.0"]]
+  :dependencies [[org.clojure/clojure "1.4.0"]
+                 [com.novemberain/monger "1.0.0-beta7"]
+                 [clj-time "0.4.2"]
+                 [org.clojure/data.json "0.1.2"]]
   :main red-panda.core)
