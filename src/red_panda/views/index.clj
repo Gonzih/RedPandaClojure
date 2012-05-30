0becd52ba80481aa73aea6160e1b4c2ceb8e9ac2
Max Gonzih <gonzih@gmail.com>
2012-05-30 12:33:20 +0300

    fix index page and current channel highlighting

--- src/red_panda/views/index.clj
+++ src/red_panda/views/index.clj
@@ -2,12 +2,14 @@
   (:require [red-panda.views.common :as common]
             [red-panda.views.messages :as msg-templates]
             [red-panda.messages :as messages]
-            [red-panda.irc-server :as irc])
+            [red-panda.irc-server :as irc]
+            [noir.session :as session])
   (:use [noir.core :only [defpage]]
         [hiccup.core :only [html]]))
 
 (defpage "/" []
          (let [channel (first irc/channels)]
+           (session/put! :channel channel)
            (common/layout
              (msg-templates/messages (messages/get-messages channel)
                                      channel
