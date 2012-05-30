f7b70339ab978dc993f2ac8706e13f871988718e
Max Gonzih <gonzih@gmail.com>
2012-05-30 22:38:30 +0300

    fix typo

--- src/red_panda/http_server.clj
+++ src/red_panda/http_server.clj
@@ -1,7 +1,7 @@
 (ns red-panda.http-server
   (:require [noir.server :as http]))
 
-(defn start [mone]
+(defn start [mode]
   (http/load-views "src/red_panda/views/")
   (let [port (Integer. (get (System/getenv) "PORT" "8080"))]
     (http/start port {:mode mode
