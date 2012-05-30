6ab1033a9e54204f12fb4e850e69b5e3dfb2abe2
Max Gonzih <gonzih@gmail.com>
2012-05-30 10:11:14 +0300

    rendering channels

--- src/red_panda/views/common.clj
+++ src/red_panda/views/common.clj
@@ -1,6 +1,7 @@
 (ns red-panda.views.common
   (:use [noir.core :only [defpartial]]
-        [hiccup.page-helpers :only [include-css html5]]))
+        [hiccup.page-helpers :only [include-css html5]]
+        [red-panda.views.channels :as chan-templates]))
 
 (defpartial layout [& content]
             (html5
@@ -8,5 +9,7 @@
                 [:title "red-panda"]
                   (include-css "/css/bootstrap.min.css")]
               [:body
-                [:div.container
-                  content]]))
+                [:div.container-fluid
+                 [:div.row-fluid
+                  [:div.span2 (chan-templates/render-channels)]
+                  [:div.span10 content]]]]))
