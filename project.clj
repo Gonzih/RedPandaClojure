084df8aa7c41de81b5c979cefb5dff714223d89f
Max Gonzih <gonzih@gmail.com>
2012-07-30 17:10:57 +0300

    update clojure to 1.4 and monger to 1.1.2

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject red-panda "1.0.0-SNAPSHOT"
   :description "IRC bot written in clojure"
-  :dependencies [[org.clojure/clojure "1.3.0"]
-                 [com.novemberain/monger "1.0.0-beta7"]
+  :dependencies [[org.clojure/clojure "1.4.0"]
+                 [com.novemberain/monger "1.1.2"]
                  [clj-time "0.4.2"]
                  [org.clojure/data.json "0.1.2"]
                  [noir "1.2.2"]]
