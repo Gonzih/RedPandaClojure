874b18a377810dc2a56ca6ecd511079e41cc4a97
Max Gonzih <gonzih@gmail.com>
2012-07-31 11:58:58 +0300

    remove cljs from resources

--- resources/public/cljs/red_panda/cljs/red-panda.js
+++
@@ -1,3 +0,0 @@
-goog.provide('red_panda.core');
-goog.require('cljs.core');
-goog.require('jayq.core');
--- resources/public/cljs/red_panda/cljs/red_panda.js
+++
@@ -1,5 +0,0 @@
-goog.provide('red_panda.core');
-goog.require('cljs.core');
-goog.require('jayq.util');
-goog.require('jayq.core');
-jayq.util.log.call(null,jayq.core.$.call(null,"\uFDD0'.body"));
