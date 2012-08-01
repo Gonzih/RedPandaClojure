e8d1f157692fa4c47ad2469e67817fe85209d7d7
Max Gonzih <gonzih@gmail.com>
2012-08-01 22:29:48 +0300

    update jayq version

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
                  [congomongo "0.1.10"]
                  [clj-time "0.4.4"]
                  [noir-cljs "0.3.0"]
-                 [jayq "0.1.0-alpha2"]
+                 [jayq "0.1.0-alpha4"]
                  [org.webbitserver/webbit "0.4.14"]
                  [org.clojure/data.json "0.1.3"]
                  [noir "1.3.0-beta10"]]
