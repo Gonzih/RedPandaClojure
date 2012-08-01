19e57ba0ce8dc12ca934d7024cb456aa09c9943f
Max Gonzih <gonzih@gmail.com>
2012-08-01 11:40:23 +0300

    use db provided by url

--- src/red_panda/messages.clj
+++ src/red_panda/messages.clj
@@ -3,9 +3,8 @@
   (:require [somnium.congomongo :as mongo])
   (:use [red-panda.util]))
 
-(let [uri (or (System/getenv "MONGOHQ_URL") (System/getenv "MONGOLAB_URI") (System/getenv "MONGO_URI") "mongodb://localhost")]
-  (-> (mongo/make-connection uri) mongo/set-connection!)
-  (mongo/set-database! "red-panda"))
+(let [uri (or (System/getenv "MONGOHQ_URL") (System/getenv "MONGOLAB_URI") (System/getenv "MONGO_URI") "mongodb://localhost/red-panda")]
+  (-> (mongo/make-connection uri) mongo/set-connection!))
 
 
 (def coll "messages")
