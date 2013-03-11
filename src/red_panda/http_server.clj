9f0b1558916e43986284107bc1b5f71fe98dfbce
Max Gonzih <gonzih@gmail.com>
2013-03-11 04:38:12 +0300

    Show port is running on

--- src/red_panda/http_server.clj
+++ src/red_panda/http_server.clj
@@ -7,6 +7,7 @@
 (defn start [mode]
   (http/load-views "src/red_panda/views/")
   (let [port (Integer. (get (System/getenv) "PORT" "3000"))]
+    (println (str "Running on port: " port))
     (noir.cljs.core/start mode cljs-options)
     (http/start port {:mode mode
                       :ns 'red-panda})))
